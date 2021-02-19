ce203b12f814eba54448ab8dc9624062590fa21c
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2021-02-19 17:32:33 -0300

    Add missing annotation

--- src/main/java/com/gutotech/fatecandoapi/model/assembler/InstitutionModelAssembler.java
+++ src/main/java/com/gutotech/fatecandoapi/model/assembler/InstitutionModelAssembler.java
@@ -5,15 +5,16 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
 import com.gutotech.fatecandoapi.model.Institution;
 import com.gutotech.fatecandoapi.rest.InstitutionRestController;
 
+@Component
 public class InstitutionModelAssembler implements RepresentationModelAssembler<Institution, EntityModel<Institution>> {
 
 	@Override
 	public EntityModel<Institution> toModel(Institution institution) {
-
 		return EntityModel.of(institution, //
 				linkTo(methodOn(InstitutionRestController.class).getInstitution(institution.getId())).withSelfRel(),
 				linkTo(methodOn(InstitutionRestController.class).getAllInstitutions()).withRel("institutions"));

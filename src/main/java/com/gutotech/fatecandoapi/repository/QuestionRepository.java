32cf0beb04e646ae20a4c4e266ffd4b71fbd0508
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2021-04-05 03:08:53 -0300

    Add method to get random shuffled questions

--- src/main/java/com/gutotech/fatecandoapi/repository/QuestionRepository.java
+++ src/main/java/com/gutotech/fatecandoapi/repository/QuestionRepository.java
@@ -6,11 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.gutotech.fatecandoapi.model.Question;
+import com.gutotech.fatecandoapi.model.QuestionType;
 import com.gutotech.fatecandoapi.model.Topic;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-	
+
 	List<Question> findAllByTopic(Topic topic);
+
+	List<Question> findByTypeAndTopicIn(QuestionType type, List<Topic> topics);
 
 }
--- src/main/java/com/gutotech/fatecandoapi/service/QuestionService.java
+++ src/main/java/com/gutotech/fatecandoapi/service/QuestionService.java
@@ -1,11 +1,14 @@
 package com.gutotech.fatecandoapi.service;
 
+import java.security.SecureRandom;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gutotech.fatecandoapi.model.Question;
+import com.gutotech.fatecandoapi.model.QuestionType;
 import com.gutotech.fatecandoapi.model.Topic;
 import com.gutotech.fatecandoapi.repository.QuestionRepository;
 import com.gutotech.fatecandoapi.rest.ResourceNotFoundException;
@@ -22,6 +25,22 @@ public class QuestionService {
 
 	public List<Question> findAllByTopic(Topic topic) {
 		return repository.findAllByTopic(topic);
+	}
+
+	public List<Question> getRandomQuestions(QuestionType type, List<Topic> topics, int quantity) {
+		List<Question> questions = findByTypeAndTopicIn(type, topics);
+
+		Collections.shuffle(questions, new SecureRandom());
+
+		if (questions.size() > quantity) {
+			return questions.subList(0, quantity);
+		}
+
+		return questions;
+	}
+
+	public List<Question> findByTypeAndTopicIn(QuestionType type, List<Topic> topics) {
+		return repository.findByTypeAndTopicIn(type, topics);
 	}
 
 	public Question findById(Long id) {

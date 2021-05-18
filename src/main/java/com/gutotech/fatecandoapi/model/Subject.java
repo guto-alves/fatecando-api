c1a5ed3b5d747682f38b2d9659bc9a70f70bd966
Gustavo Alves <gustavo.almeida13@fatec.sp.gov.br>
2021-05-17 21:13:05 -0300

    Add OrderBy annotation to Forum Topics list

--- src/main/java/com/gutotech/fatecandoapi/model/Subject.java
+++ src/main/java/com/gutotech/fatecandoapi/model/Subject.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
@@ -52,6 +53,7 @@ public class Subject {
 
 	@JsonIgnore
 	@OneToMany(mappedBy = "subject", cascade = CascadeType.ALL)
+	@OrderBy("creationDate DESC")
 	private List<ForumThread> forumThreads = new ArrayList<>();
 
 	@ManyToOne

9d67e1d327dcb60389f27d4dd8b51ca75fec9665
Gustavo Alves <gustavo.almeida13@fatec.sp.gov.br>
2021-06-12 15:22:10 -0300

    Fix UserModelAssembler links

--- src/main/java/com/gutotech/fatecandoapi/model/assembler/UserModelAssembler.java
+++ src/main/java/com/gutotech/fatecandoapi/model/assembler/UserModelAssembler.java
@@ -8,7 +8,7 @@ import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
 import com.gutotech.fatecandoapi.model.User;
-import com.gutotech.fatecandoapi.rest.CourseRestController;
+import com.gutotech.fatecandoapi.rest.UserRestController;
 
 @Component
 public class UserModelAssembler implements RepresentationModelAssembler<User, EntityModel<User>> {
@@ -16,8 +16,7 @@ public class UserModelAssembler implements RepresentationModelAssembler<User, En
 	@Override
 	public EntityModel<User> toModel(User user) {
 		return EntityModel.of(user, //
-				linkTo(methodOn(CourseRestController.class).getCourse(user.getId())).withSelfRel(),
-				linkTo(methodOn(CourseRestController.class).getAllCourses()).withRel("users"));
+				linkTo(methodOn(UserRestController.class).getUser(user.getId())).withSelfRel());
 	}
 
 }
